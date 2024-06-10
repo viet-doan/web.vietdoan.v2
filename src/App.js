df31a623362512dd3b762f670a4f41fa92d54e08
vi <vietdoan.vqd@gmail.com>
2024-06-11 04:18:34 +1000

    Layout for Landing Page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import Landing from './Landing';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="cr-container">
+        <p class="copyright">Copyright 2024 @ Viet Doan</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
