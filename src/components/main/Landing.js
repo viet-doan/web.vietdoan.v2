0c278cf998645240f6d48b7999d0c70ac620d1b6
vi <vietdoan.vqd@gmail.com>
2024-06-17 01:59:00 +1000

    Adding Koi Corner Design Gallery Link

--- src/components/main/Landing.js
+++ src/components/main/Landing.js
@@ -12,9 +12,10 @@ const sections = {
 };
 
 function Landing() {
-  const oldPortfolio = "http://anai-tbgp8ueihwl7eqbikbgy.s3-website-ap-southeast-2.amazonaws.com/index.html";
+  const OldPortfolioURL = "http://anai-tbgp8ueihwl7eqbikbgy.s3-website-ap-southeast-2.amazonaws.com/index.html";
+  const KoiCornerURL = "https://koicorner.art/";
   const arrow = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/></svg>
-
+  
   return (
     <main className="main row">
       <div className="desktop">
@@ -37,12 +38,17 @@ function Landing() {
         <p>I am currently a Computer Science student at the University of Melbourne.
           <br></br>And this website is my first React Project.</p>
 
-        <Link className='links' to='/About'>
+        {/* <Link className='links' to='/About'>
           More about me 
           <span className='icon-arrow'>{arrow}</span>
-        </Link>
+        </Link> */}
         
-        <a className='links' href={oldPortfolio} target='_blank' rel="noreferrer">
+        <a className='links' href={KoiCornerURL} target='_blank' rel="noreferrer">
+        My Design Gallery
+          <span className='icon-arrow'>{arrow}</span>
+        </a>
+
+        <a className='links' href={OldPortfolioURL} target='_blank' rel="noreferrer">
         My Old Online Portfolio
           <span className='icon-arrow'>{arrow}</span>
         </a>
